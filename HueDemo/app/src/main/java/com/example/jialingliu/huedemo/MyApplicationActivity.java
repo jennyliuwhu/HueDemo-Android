c797e72b6cb6383a9365cab7b1c02b3d64378e2c
jialingliu <jialing.whu@gmail.com>
2017-04-17 19:25:58 -0700

    complete work

---
+++ HueDemo/app/src/main/java/com/example/jialingliu/huedemo/MyApplicationActivity.java
@@ -0,0 +1,108 @@
+package com.example.jialingliu.huedemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.philips.lighting.hue.listener.PHLightListener;
+import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHBridgeResource;
+import com.philips.lighting.model.PHHueError;
+import com.philips.lighting.model.PHLight;
+import com.philips.lighting.model.PHLightState;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+/**
+ * MyApplicationActivity - The starting point for creating your own Hue App.
+ * Currently contains a simple view with a button to change your lights to random colours.  Remove this and add your own app implementation here! Have fun!
+ *
+ * @author jialingliu
+ */
+public class MyApplicationActivity extends Activity {
+    private PHHueSDK phHueSDK;
+    public static final String TAG = "QuickStart";
+    private static boolean on = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.app_name);
+        setContentView(R.layout.activity_main);
+        phHueSDK = PHHueSDK.create();
+        Button randomButton;
+        randomButton = (Button) findViewById(R.id.buttonRand);
+        randomButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                controlLights();
+            }
+
+        });
+
+    }
+
+    public void controlLights() {
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+        System.out.println("change light color start");
+        List<PHLight> allLights = bridge.getResourceCache().getAllLights();
+        System.out.println("we have " + allLights.size() + " lights");
+        Random rand = new Random();
+
+        PHLight light = allLights.get(1); // get first light
+        PHLightState lightState = new PHLightState();
+        System.out.println(on);
+        if (!on) {
+            lightState.setOn(true);
+            on = true;
+        } else {
+            lightState.setOn(false);
+            on = false;
+        }
+        bridge.updateLightState(light, lightState, listener);
+    }
+    // If you want to handle the response from the bridge, create a PHLightListener object.
+    PHLightListener listener = new PHLightListener() {
+
+        @Override
+        public void onSuccess() {
+        }
+
+        @Override
+        public void onStateUpdate(Map<String, String> arg0, List<PHHueError> arg1) {
+            Log.w(TAG, "Light has updated");
+        }
+
+        @Override
+        public void onError(int arg0, String arg1) {}
+
+        @Override
+        public void onReceivingLightDetails(PHLight arg0) {}
+
+        @Override
+        public void onReceivingLights(List<PHBridgeResource> arg0) {}
+
+        @Override
+        public void onSearchComplete() {}
+    };
+
+    @Override
+    protected void onDestroy() {
+        PHBridge bridge = phHueSDK.getSelectedBridge();
+        if (bridge != null) {
+
+            if (phHueSDK.isHeartbeatEnabled(bridge)) {
+                phHueSDK.disableHeartbeat(bridge);
+            }
+
+            phHueSDK.disconnect(bridge);
+            super.onDestroy();
+        }
+    }
+}
